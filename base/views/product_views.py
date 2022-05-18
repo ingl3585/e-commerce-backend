32d050293af52ec8f66b54d354dbad190df46c3e
Tony Ingle <ingleanthonyco@gmail.com>
2022-05-18 10:34:22 -0600

    added reviews, search, and pagination

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,15 +1,31 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
-from base.models import Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from rest_framework import status
+from base.models import Product, Review
 from base.products import products
 from base.serializers import ProductSerializer
 
 @api_view(['GET'])
 def getProducts(request):
-    products = Product.objects.all()
+    query = request.query_params.get('keyword')
+    if query == None:
+        query = ''
+    products = Product.objects.filter(name__icontains=query)
+    page = request.query_params.get('page')
+    paginator = Paginator(products, 5)
+    try: 
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    if page == None:
+        page = 1
+    page = int(page)
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages})
 
 @api_view(['GET'])
 def getProduct(request, pk):
@@ -61,3 +77,35 @@ def uploadImage(request):
     product.image = request.FILES.get('image')
     product.save()
     return Response('Image was uploaded.')
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects.get(id=pk)
+    data = request.data
+
+    alreadyExits = product.review_set.filter(user=user).exists()
+    if alreadyExits:
+        content = {'detail': 'Product already reviewed.'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    elif data['rating.rate'] == 0:
+        content = {'detail': 'Please select a rating.'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        review = Review.objects.create(
+            user = user,
+            product= product,
+            name = user.first_name,
+            rating = data['rating'],
+            comment = data['comment'],
+        )
+        reviews = product.review_set.all()
+        product.numReviews = len(reviews)
+        total = 0
+        for i in reviews:
+            total += i.rating
+        product.rating = total / len(reviews)
+        product.save()
+        return Response('Review was added.')
